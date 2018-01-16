ef4b3003414de6df0fd2f27acd3427c14d2396fe
Natalee C <COPENATALEE@ME.COM>
2018-01-16 17:29:55 -0500

    MVP

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+
+import SearchContainer from './SearchContainer'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='header'>
+        <h1>React TV Browser</h1>
+        <SearchContainer />
       </div>
-    );
+    )
   }
 }
 
